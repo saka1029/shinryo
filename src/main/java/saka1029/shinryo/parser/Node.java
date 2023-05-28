803d76338c1af93d938a4672000f3026f3fac350
saka1029 <saka1029@gmail.com>
2023-05-28 22:07:10 +0900

    ああ

--- src/main/java/saka1029/shinryo/parser/Node.java
+++ src/main/java/saka1029/shinryo/parser/Node.java
@@ -2,12 +2,26 @@ package saka1029.shinryo.parser;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public record Node(
     Token token,
     List<Node> children) {
-    
+
     public Node(Token token) {
         this(token, new ArrayList<>());
+    }
+
+    public void print(Consumer<String> callback) {
+        new Object() {
+            void print(Node node, int level) {
+                if (node.token() == null)
+                    callback.accept("ROOT");
+                else
+                    callback.accept("  ".repeat(level) + node.token());
+                for (Node child : node.children())
+                    print(child, level + 1);
+            }
+        }.print(this, 0);
     }
 }
--- src/test/java/saka1029/shinryo/parser/TestTKParser.java
+++ src/test/java/saka1029/shinryo/parser/TestTKParser.java
@@ -1,6 +1,8 @@
 package saka1029.shinryo.parser;
 
 import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Logger;
 
 import org.junit.Test;
@@ -11,21 +13,15 @@ public class TestTKParser {
 
     static { Logging.init(); } 
     static final Logger logger = Logger.getLogger(TestTKParser.class.getSimpleName());
-
-    static void print(Node parent, int level) {
-        if (parent.token() == null)
-            logger.info("ROOT");
-        else
-            logger.info("  ".repeat(level) + parent.token());
-        for (Node child : parent.children())
-            print(child, level + 1);
-    }
+    
+    static final PrintStream OUT = new PrintStream(System.out, true, StandardCharsets.UTF_8);
 
     @Test
     public void testParse() throws IOException {
         String inTxtFile = "data/04-t-kokuji.txt";
         Node root = new TKParser().parse(inTxtFile);
-        print(root, 0);
+//        root.print(logger::info);
+        root.print(OUT::println);
     }
 
 }
