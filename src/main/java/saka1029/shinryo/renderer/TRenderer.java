55d0de0747fd3ab15037ff2c2bbe87c64f9a29e4
saka1029 <saka1029@gmail.com>
2023-06-02 13:32:21 +0900

    Rendererでファイル名の重複チェックを行うようにした。

--- src/main/java/saka1029/shinryo/renderer/TRenderer.java
+++ src/main/java/saka1029/shinryo/renderer/TRenderer.java
@@ -7,16 +7,16 @@ import saka1029.shinryo.parser.Node;
 import saka1029.shinryo.parser.TKParser;
 
 public class TRenderer extends Renderer {
-        public TRenderer(String outDir) throws IOException {
-            super(outDir);
-        }
+	public TRenderer(String outDir) throws IOException {
+		super(outDir);
+	}
 
-        @Override
-        void render(Node node, int level, TextWriter w) throws IOException {
-            if (node.token.type == TKParser.区分番号 && node.children.size() > 0)
-                writeLinkKubun(node, level, w);
-            else
-                writeLine(node, level, w);
-        }
+	@Override
+	void render(Node node, int level, TextWriter w) throws IOException {
+		if (node.token.type == TKParser.区分番号 && node.children.size() > 0)
+			writeLinkKubun(node, level, w);
+		else
+			writeLine(node, level, w);
+	}
 
 }
