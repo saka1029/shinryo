a7f23394bfa42953e36439308aba259f7e677f1d
saka1029 <saka1029@gmail.com>
2023-06-18 18:26:46 +0900

    PDFBox追加。

---
+++ src/main/java/saka1029/shinryo/pdf/PDF.java
@@ -0,0 +1,306 @@
+package saka1029.shinryo.pdf;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+
+import org.apache.pdfbox.multipdf.Splitter;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.TextPosition;
+
+import saka1029.shinryo.common.Common;
+
+public class PDF {
+
+    static final Logger LOGGER = Common.logger(PDF.class);
+    static {
+        // PDFBox関連のログを抑制します。
+        Logger.getLogger("org.apache").setLevel(Level.SEVERE);
+    }
+    static final PrintStream OUT = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+    static final Charset 出力文字セット = StandardCharsets.UTF_8;
+    static final String 改行文字 = "\n";
+
+    /**
+     * A4のポイントサイズ 横約8.27 × 縦約11.69 インチ 595.44 x 841.68 ポイント
+     */
+    public static final float PAGE_WIDTH = 596F, PAGE_HEIGHT = 842F;
+
+    public final boolean horizontal;
+
+    public float 行併合範囲割合 = 0.6F;
+    public float ルビ割合 = 0.6F;
+    public float 行高さ規定値 = 10F;
+    public float 行間隔規定値 = 14F;
+    public Pattern ルビパターン = Pattern.compile("\\p{IsHiragana}*");
+    public Pattern ページ番号パターン = Pattern.compile("^\\s*\\S*\\s*-\\s*\\d+\\s*-\\s*$");
+    public DebugElement debugElement = null;
+
+    public PDF(boolean horizontal) {
+        this.horizontal = horizontal;
+    }
+
+    public record Element(float x, float y, float fontSize, String text) {
+        @Override
+        public String toString() {
+            return "%sx%s:%s:%s".formatted(x, y, fontSize, text);
+        }
+    }
+
+    public interface DebugElement {
+        void element(String path, int pageNo, int lineNo, 文書属性 attr, TreeSet<Element> elements);
+    }
+
+    static final Comparator<Element> 行内ソート = Comparator.comparing(Element::x)
+        .thenComparing(Comparator.comparing(Element::y).reversed());
+
+    List<TreeMap<Float, List<Element>>> 行分割(List<List<Element>> fileElements) {
+        List<TreeMap<Float, List<Element>>> result = new ArrayList<>();
+        for (List<Element> pageElements : fileElements) {
+            TreeMap<Float, List<Element>> lineElements = new TreeMap<>();
+            result.add(lineElements);
+            for (Element e : pageElements)
+                lineElements.computeIfAbsent(e.y, k -> new ArrayList<>()).add(e);
+        }
+        return result;
+    }
+
+    public record 文書属性(boolean 横書き, float 左余白, float 行間隔, float 行高さ, float 行併合範囲, float ルビ高さ) {
+    }
+
+    文書属性 文書属性(List<TreeMap<Float, List<Element>>> fileLines) {
+        float 左余白 = Float.MAX_VALUE;
+        Map<Float, Integer> 行間隔度数分布 = new HashMap<>();
+        Map<Float, Integer> 行高さ度数分布 = new HashMap<>();
+        for (TreeMap<Float, List<Element>> pageElements : fileLines) {
+            float prevY = Float.MIN_VALUE;
+            for (Entry<Float, List<Element>> line : pageElements.entrySet()) {
+                左余白 = Math.min(左余白, line.getValue().get(0).x);
+                float y = line.getKey();
+                if (prevY != Float.MIN_VALUE)
+                    行間隔度数分布.compute(y - prevY, (k, v) -> v == null ? 1 : v + 1);
+                prevY = y;
+                for (Element e : line.getValue())
+                    行高さ度数分布.compute(e.fontSize, (k, v) -> (v == null ? 0 : v) + e.text.length());
+            }
+        }
+        if (左余白 == Float.MAX_VALUE)
+            左余白 = 0;
+        float 行間隔 = 行間隔度数分布.entrySet().stream()
+            .max(Entry.comparingByValue())
+            .map(Entry::getKey)
+            .orElse(行間隔規定値);
+        float 行高さ = 行高さ度数分布.entrySet().stream()
+            .max(Entry.comparingByValue())
+            .map(Entry::getKey)
+            .orElse(行高さ規定値);
+        float 行併合範囲 = 行間隔 * 行併合範囲割合;
+        float ルビ高さ = 行高さ * ルビ割合;
+        return new 文書属性(horizontal, 左余白, 行間隔, 行高さ, 行併合範囲, ルビ高さ);
+    }
+
+    float round(float value) {
+        return Math.round(value);
+    }
+
+    void parse(PDDocument doc, List<List<Element>> fileElements, int pageNo) throws IOException {
+        List<Element> pageElements = new ArrayList<>();
+        fileElements.add(pageElements);
+        new PDFTextStripper() {
+            {
+                setStartPage(pageNo);
+                setEndPage(pageNo);
+                getText(doc);
+            }
+
+            @Override
+            protected void processTextPosition(TextPosition text) {
+                if (text.getUnicode().isBlank())
+                    return;
+                Element element = new Element(
+                    round(horizontal ? text.getX() : text.getY()),
+                    round(horizontal ? text.getY() : PAGE_WIDTH - text.getX()),
+                    round(text.getXScale()),
+                    text.getUnicode());
+                pageElements.add(element);
+            }
+        };
+    }
+
+    String toString(TreeSet<Element> line, float leftMargin, float charWidth) {
+        StringBuilder sb = new StringBuilder();
+        float halfWidth = charWidth / 2;
+        float start = leftMargin;
+        for (Element e : line) {
+            int spaces = Math.round((e.x - start) / halfWidth);
+            for (int i = 0; i < spaces; ++i)
+                sb.append(" ");
+            sb.append(e.text);
+            start = e.x + (float) e.text.codePoints().mapToDouble(c -> c < 256 ? halfWidth : charWidth).sum();
+        }
+        return sb.toString();
+    }
+
+    void addLine(List<String> list, TreeSet<Element> sortedLine, String path, int pageNo, int lineNo, 文書属性 文書属性) {
+        if (!sortedLine.isEmpty())
+            list.add(toString(sortedLine, 文書属性.左余白, 文書属性.行高さ));
+        if (debugElement != null)
+            debugElement.element(path, pageNo, lineNo, 文書属性, sortedLine);
+        sortedLine.clear();
+    }
+
+    public List<List<String>> read(String inPdfPath) throws IOException {
+        List<List<String>> result = new ArrayList<>();
+        PDDocument doc = PDDocument.load(new File(inPdfPath));
+        int numberOfPages = doc.getNumberOfPages();
+        List<List<Element>> fileElements = new ArrayList<>();
+        try (Closeable c = () -> doc.close()) {
+            for (int i = 0; i < numberOfPages; ++i)
+                parse(doc, fileElements, i + 1);
+        }
+        List<TreeMap<Float, List<Element>>> fileLines = 行分割(fileElements);
+        文書属性 文書属性 = 文書属性(fileLines);
+        LOGGER.info(inPdfPath + ":" + 文書属性);
+        int pageNo = 0;
+        for (TreeMap<Float, List<Element>> lines : fileLines) {
+            ++pageNo;
+            List<String> linesString = new ArrayList<>();
+            result.add(linesString);
+//			float y = Float.MIN_VALUE;
+            TreeSet<Element> sortedLine = new TreeSet<>(行内ソート);
+            int lineNo = 0;
+            for (Entry<Float, List<Element>> line : lines.entrySet()) {
+                List<Element> lineElements = line.getValue();
+                if (lineElements.stream().allMatch(e -> e.fontSize <= 文書属性.ルビ高さ && ルビパターン.matcher(e.text).matches()))
+                    continue;
+//				if (y != Float.MIN_VALUE && line.getKey() > y + 文書属性.行併合範囲)
+                if (!sortedLine.isEmpty()
+                    && line.getKey() > sortedLine.stream().mapToDouble(Element::y).average().getAsDouble() + 文書属性.行併合範囲)
+                    addLine(linesString, sortedLine, inPdfPath, pageNo, ++lineNo, 文書属性);
+                sortedLine.addAll(lineElements);
+            }
+            addLine(linesString, sortedLine, inPdfPath, pageNo, ++lineNo, 文書属性);
+        }
+        return result;
+    }
+
+    public void テキスト変換(String outTxtFile, String... inPdfFiles) throws IOException {
+        Files.createDirectories(Path.of(outTxtFile).getParent());
+        try (PrintWriter writer = new PrintWriter(new FileWriter(outTxtFile, 出力文字セット))) {
+            for (String path : inPdfFiles) {
+                List<List<String>> pages = read(path);
+                for (int i = 0, pageSize = pages.size(); i < pageSize; ++i) {
+                    writer.printf("# file: %s page: %d%s", Path.of(path).getFileName(), i + 1, 改行文字);
+                    for (String line : pages.get(i))
+                        writer.printf("%s%s", ページ番号パターン.matcher(line).replaceFirst("#$0"), 改行文字);
+                }
+            }
+        }
+    }
+
+//    public int 様式名出現最大行 = 3;
+//    public Pattern 様式IDパターン = Pattern.compile(
+//        "\\s*[（(]?(" // group1:別紙様式3の4の5
+//            + "\\s*(?:(?:別\\s*紙)?\\s*様\\s*式|別\\s*添|別\\s*紙)"
+//            + "((?:\\s*\\d)+)" // group2:3
+//            + "(?:\\s*の((?:\\s*\\d)+))?" // group3:4
+//            + "(?:\\s*の((?:\\s*\\d)+))?" // group4:5
+//            + ")\\s*[）)]?"
+//            + "(?:\\s+(.*))?"); // group5:様式名
+//
+//    public void 様式一覧変換(String outFile, String... inFiles) throws IOException {
+//        try (PrintWriter writer = new PrintWriter(outFile, 出力文字セット)) {
+//            for (String inFile : inFiles) {
+//                List<List<String>> pages = read(inFile);
+//                writer.printf("#file %s%s", inFile, 改行文字);
+//                String name = null, id = null, title = null;
+//                int startPage = -1;
+//                int i = 0;
+//                for (int pageSize = pages.size(); i < pageSize; ++i) {
+//                    List<String> page = pages.get(i);
+//                    for (int j = 0, maxLine = Math.min(様式名出現最大行, page.size()); j < maxLine; ++j) {
+//                        String line = page.get(j);
+//                        OUT.printf("%d:%d:%s%n", i + 1, j + 1, line);
+//                        String normalLine = Normalizer.normalize(line, Form.NFKD);
+//                        Matcher m = 様式IDパターン.matcher(normalLine);
+//                        if (m.matches()) {
+//                            if (name != null)
+//                                writer.printf("%s,%s,%d,%d,%s%s", name, id, startPage, i, title, 改行文字);
+//                            name = m.group(1).replaceAll("\\s+", "");
+//                            id = m.group(2);
+//                            startPage = i + 1;
+//                            for (int k = 3; k <= 5 && m.group(k) != null; ++k)
+//                                id += "_" + m.group(k);
+//                            id = id.replaceAll("\\s+", "");
+//                            title = m.group(5);
+//                            if (title == null && j + 1 < page.size())
+//                                title = page.get(j + 1);
+//                            title = title.replaceAll("\\s+", "");
+//                        }
+//                    }
+//                }
+//                if (name != null)
+//                    writer.printf("%s,%s,%d,%d,%s%s", name, id, startPage, i, title, 改行文字);
+//            }
+//        }
+//    }
+    
+    public static List<様式> ページ分割(String inTextFile, String outDir) throws IOException {
+        Files.createDirectories(Path.of(outDir));
+        List<様式> result = new ArrayList<>();
+        try (BufferedReader reader = Files.newBufferedReader(Path.of(inTextFile), 出力文字セット)) {
+            PDDocument doc = null;
+            String line, curFile = null;
+            while ((line = reader.readLine()) != null) {
+                if (line.isBlank() || line.startsWith("#"))
+                    continue;
+                    String[] fields = line.split(",", 6);
+                    String file = fields[0];
+                    String name = fields[1];
+                    String id = fields[2];
+                    String outFile = /* outPdfPrefix + */ id + ".pdf";
+                    String title = fields[5];
+                    int startPage = Integer.parseInt(fields[3]);
+                    int endPage = Integer.parseInt(fields[4]);
+                    result.add(new 様式(file, name, id, startPage, endPage, title));
+                    if (!file.equals(curFile)) {
+                        if (doc != null)
+                            doc.close();
+                        doc = PDDocument.load(new File(file));
+                        curFile = file;
+                    }
+                    Splitter splitter = new Splitter();
+                    splitter.setStartPage(startPage);
+                    splitter.setEndPage(endPage);
+                    splitter.setSplitAtPage(doc.getNumberOfPages());
+                    List<PDDocument> splitted = splitter.split(doc);
+                    splitted.get(0).save(outDir + "/" + outFile);
+                    for (PDDocument d : splitted)
+                        d.close();
+            }
+            if (doc != null)
+                doc.close();
+        }
+        return result;
+    }
+}
---
+++ src/main/java/saka1029/shinryo/pdf/様式.java
@@ -0,0 +1,168 @@
+package saka1029.shinryo.pdf;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.Normalizer;
+import java.text.Normalizer.Form;
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 様式名から様式IDへの変換規則は以下の通りです。
+ * 施設基準の場合は「別添xの様式yのz」のような形式になります。
+ * <pre>
+ * 別紙様式aのbのc → SYa_b_c
+ * 別添aのbのc → Ta_b_c
+ * 別紙aのbのc → Sa_b_c
+ * 様式aのbのc → Ya_b_c
+ * 別添6の別紙aのbのc → * T6_Sa_b_c
+ * 別添7の様式aのbのc → T7_Ya_b_c
+ * 別添2の様式aのbのc → T2_Ya_b_c
+ * </pre>
+ * @param name 様式名。ex.「別紙様式2の3」
+ * @param id 様式ID。ex.「SY2_3」
+ * @param startPage 開始ページ。ex.「123」
+ * @param endPage 終了ページ。ex.「124」
+ * @param title 様式タイトル。ex.「退院証明書」
+ */
+public record 様式(String file, String name, String id, int startPage, int endPage, String title) {
+
+	static final Logger logger = Logger.getLogger(様式.class.getName());
+    static final int 様式名出現最大行 = 3;
+
+    /**
+     * 様式名を様式IDに変換します。
+     */
+    public static String id(String name) {
+        String r =  Normalizer.normalize(name, Form.NFKD)
+			.replaceAll("\\s+", "")
+			.replaceAll("別添", "T").replaceAll("別紙", "S").replaceAll("様式", "Y")
+            .replaceAll("[のー－]", "_");
+        return r;
+    }
+
+    static final Pattern 施設基準様式名パターン = Pattern.compile("\\s*[(（]?\\s*(" // 様式名 (group1)
+        + "\\s*(?:別\\s*添|別\\s*紙|様\\s*式)"
+        + "\\s*(?:(?:\\d|\\s+)+)" // 様式番号1
+        + "\\s*(?:の\\s*(?:(?:\\d|\\s+)+))?" // 様式番号2
+        + "\\s*(?:の\\s*(?:(?:\\d|\\s+)+))?" // 様式番号3
+        + ")\\s*[)）]?(?:\\s+(.+))?"); // 様式タイトル (group2)
+
+    /**
+     * 施設基準の様式PDFファイルから様式一覧を抽出してテキストファイルに出力します。
+     * <p>
+     * 〇施設基準の様式に関わる文書構成(令和4年度)
+     * <p>
+     * 
+     * <pre>
+     * 施設基準告示
+     *      基本診療料(000907989.pdf)
+     *          別添1 初・再診料の施設基準                          本文
+     *          別添2 入院基本料等の施設基準                        本文
+     *          別添3 入院基本料等加算の施設基準                    本文
+     *          別添4 特定入院料の施設基準                          本文
+     *          別添5 短期滞在手術等基本料の施設基準                本文
+     *          別添6 診療等に要する書面等                          本文
+     *              別紙1, 2, ...                                   診療等に要する様式
+     *          別添7 基本診療料の施設基準等に係る届出書            届出書様式
+     *          別添7の2 基本診療料の施設基準等に係る届出書         届出書様式
+     *              様式1, 1の2, ...                                届出書様式
+     *      特掲診療料(000907862.pdf)
+     *          別添1 特掲診療料等の施設基準                        本文
+     *          別添2 特掲診療料の施設基準に係る届出書              届出書様式
+     *              様式1, 1の2, ...                                届出書様式
+     *          別添2の2 特掲診療料の施設基準等に係る届出書         届出書様式
+     * </pre>
+     * 
+     * 基本診療料の様式は別紙nと様式nの2種類がある。 特掲診療料の様式は様式nのみである。 別添nは基本的に本文の見出しである。
+     * ただし、基本診療料の別添7および別添7の2、 特掲診療料の別添2および別添2の2はそれぞれ独立した様式である。
+     * <p>
+     * 〇PDFファイル名
+     * <p>
+     * 
+     */
+    public static void 施設基準様式一覧変換(String outTxtFile, String... inPdfFiles) throws IOException {
+        Files.createDirectories(Path.of(outTxtFile).getParent());
+        try (PrintWriter out = new PrintWriter(outTxtFile, StandardCharsets.UTF_8)) {
+            for (String inPdfFile : inPdfFiles) {
+//                out.printf("#file %s\n", inPdfFile);
+                List<List<String>> pageLines = new PDF(true).read(inPdfFile);
+                String betten = ""; // 直前の「別添n」
+                int startPage = -1;
+                String name = null, id = null, title = null;
+                int i = 0;
+                for (int pageCount = pageLines.size(); i < pageCount; ++i) {
+                    List<String> lines = pageLines.get(i);
+                    for (int j = 0, lineCount = Math.min(様式名出現最大行, lines.size()); j < lineCount; ++j) {
+                        String norm = Normalizer.normalize(lines.get(j), Form.NFKD);
+                        Matcher m = 施設基準様式名パターン.matcher(norm);
+                        if (m.matches()) {
+                            if (name != null)
+                                out.printf("%s,%s,%s,%d,%d,%s\n", inPdfFile, name, id, startPage, i, title);
+                            startPage = i + 1;
+                            name = m.group(1).replaceAll("\\s+", "");
+                            if (name.matches("別添\\d+"))
+                                betten = name;
+                            title = m.group(2);
+                            if (title == null && j + 1 < lines.size())
+                                title = lines.get(j + 1);
+                            title = title.replaceAll("\\s+", "");
+                            id = id(name.startsWith("別添") ? name : betten + "の" + name);
+                        }
+                    }
+                }
+                if (name != null)
+                    out.printf("%s,%s,%s,%d,%d,%s\n", inPdfFile, name, id, startPage, i, title);
+            }
+        }
+    }
+
+    /**
+     * 医科、歯科、調剤の様式PDFファイルから様式一覧を抽出してテキストファイルに出力します。
+     */
+    static final Pattern 様式名パターン = Pattern.compile("\\s*[(（]?\\s*(" // 様式名 (group1)
+        + "\\s*(?:別\\s*[紙添]\\s*様\\s*式)"
+        + "\\s*(?:(?:\\d|\\s+)+)"  // 様式番号1
+        + "\\s*(?:[の-]\\s*(?:(?:\\d|\\s+)+))?" // 様式番号2
+        + "\\s*(?:[の-]\\s*(?:(?:\\d|\\s+)+))?" // 様式番号3
+        + ")\\s*[)）]?(?:\\s+(.+))?"); // 様式タイトル (group2)
+
+    public static void 様式一覧変換(String outTxtFile, String... inPdfFiles) throws IOException {
+        Files.createDirectories(Path.of(outTxtFile).getParent());
+        try (PrintWriter out = new PrintWriter(outTxtFile, StandardCharsets.UTF_8)) {
+            for (String inPdfFile : inPdfFiles) {
+//                out.printf("#file %s\n", inPdfFile);
+                List<List<String>> pageLines = new PDF(true).read(inPdfFile);
+                int startPage = -1;
+                String name = null, id = null, title = null;
+                int i = 0;
+                for (int pageCount = pageLines.size(); i < pageCount; ++i) {
+                    List<String> lines = pageLines.get(i);
+                    for (int j = 0, lineCount = Math.min(様式名出現最大行, lines.size()); j < lineCount; ++j) {
+                        String norm = Normalizer.normalize(lines.get(j), Form.NFKD);
+                        Matcher m = 様式名パターン.matcher(norm);
+                        if (m.matches()) {
+                            if (name != null)
+                                out.printf("%s,%s,%s,%d,%d,%s\n", inPdfFile, name, id, startPage, i, title);
+                            startPage = i + 1;
+                            // 調剤は「別紙様式1」を間違えて「別添様式1」と記述している。
+                            name = m.group(1).replaceAll("\\s+", "").replaceAll("添", "紙");
+                            title = m.group(2);
+                            if (title == null && j + 1 < lines.size())
+                                title = lines.get(j + 1);
+                            title = title.replaceAll("\\s+", "");
+                            id = id(name);
+                        }
+                    }
+                }
+                if (name != null)
+                    out.printf("%s,%s,%s,%d,%d,%s\n", inPdfFile, name, id, startPage, i, title);
+            }
+        }
+    }
+}
