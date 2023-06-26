1df08cd8d2188c0ffaf288f23c733efd38e881ea
saka1029 <saka1029@gmail.com>
2023-06-26 22:07:40 +0900

    施設基準通知読込作成。

--- src/main/java/saka1029/shinryo/parser/施設基準通知読込.java
+++ src/main/java/saka1029/shinryo/parser/施設基準通知読込.java
@@ -7,12 +7,14 @@ public class 施設基準通知読込 extends Parser {
 
     static final Logger LOGGER = Logger.getLogger(施設基準告示読込.class.getName());
 
+    static final TokenType 基本診療料 = new TokenType("基本診療料", Pat.number("基本診療料の施設基準等"), Pat.固定値id("1"));
+    static final TokenType 特掲診療料 = new TokenType("特掲診療料", Pat.number("特掲診療料の施設基準等"), Pat.固定値id("2"));
     static final TokenType 第数字の = new TokenType("第数字の", Pat.numberHeader("第" + Pat.数字の), Pat.数字id);
-	public static final TokenType 数字の = new TokenType("数字の", Pat.numberHeader(Pat.数字の), Pat.数字id);
-	public static final TokenType 括弧数字 = new TokenType("括弧数字", Pat.numberHeader(Pat.括弧数字), Pat.数字id);
-	public static final TokenType カナ = new TokenType("カナ", Pat.numberHeader(Pat.カナ), Pat.アイウid);
-	public static final TokenType 括弧カナ = new TokenType("括弧カナ", Pat.numberHeader(Pat.括弧カナ), Pat.イロハid);
-	// /shinryo/in/02/k/pdf/t/000603890.pdf 48ページまでチェック
+	static final TokenType 別添 = new TokenType("別添", Pat.number("別添" + Pat.数字), Pat.数字id);
+	static final TokenType 数字の = new TokenType("数字の", Pat.numberHeader(Pat.数字の), Pat.数字id);
+	static final TokenType 括弧数字 = new TokenType("括弧数字", Pat.numberHeader(Pat.括弧数字), Pat.数字id);
+	static final TokenType カナ = new TokenType("カナ", Pat.numberHeader(Pat.カナ), Pat.アイウid);
+	static final TokenType 括弧カナ = new TokenType("括弧カナ", Pat.numberHeader(Pat.括弧カナ), Pat.イロハid);
 
     public 施設基準通知読込() {
         super(true);
