8e13633f4f4db81d0b3aaa95bf6aec85df2054c1
saka1029 <saka1029@gmail.com>
2024-03-26 09:47:39 +0900

    ..

--- src/main/java/saka1029/shinryo/renderer/Sitemap.java
+++ src/main/java/saka1029/shinryo/renderer/Sitemap.java
@@ -3,7 +3,6 @@ package saka1029.shinryo.renderer;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Comparator;
 import java.util.Set;
 
