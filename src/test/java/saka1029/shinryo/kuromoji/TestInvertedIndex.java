1c6dd52d6a4b07c0778e072448ae56d1cf35fecd
saka1029 <saka1029@gmail.com>
2026-01-23 19:43:14 +0900

    ..

---
+++ src/test/java/saka1029/shinryo/kuromoji/TestInvertedIndex.java
@@ -0,0 +1,45 @@
+package saka1029.shinryo.kuromoji;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.function.Consumer;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+import saka1029.shinryo.common.Common;
+import saka1029.shinryo.common.Param;
+import saka1029.shinryo.parser.Node;
+import saka1029.shinryo.parser.Token;
+import saka1029.shinryo.parser.医科告示読込;
+
+public class TestInvertedIndex {
+
+    static final Logger logger = Common.logger(TestInvertedIndex.class);
+    
+    static final Param param = Param.of("in", "debug/out", "06");
+
+    static void index(Node node, String kubun, Consumer<String> out) throws IOException {
+        if (!node.isRoot()) {
+            Token t = node.token;
+            String url = kubun != null ? kubun : (node.path + ".html");
+            if (kubun == null && t.type.name.equals("区分番号"))
+                url = kubun = node.id +".html";
+            out.accept("%s:%s %s".formatted(url, t.number, t.header + t.body.stream().collect(Collectors.joining())));
+        }
+        for (Node child : node.children)
+            index(child, kubun, out);
+    }
+
+   @Test
+    public void test医科告示読込() throws IOException {
+        logger.info(Common.methodName());
+        String inTxtFile = param.inFile("i", "txt/ke.txt");
+        String outTxtFile = param.outDir("ik-index.txt");
+        Node root = new 医科告示読込().parse(inTxtFile);
+        try (PrintWriter w = new PrintWriter(outTxtFile)) {
+            index(root, null, w::println);
+        }
+    }
+}
