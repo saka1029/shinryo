9415c00d79e48aa419823bc869e29431dcb855a4
saka1029 <saka1029@gmail.com>
2026-01-24 19:54:45 +0900

    ..

---
+++ src/test/java/saka1029/shinryo/index/TestRegex.java
@@ -0,0 +1,34 @@
+package saka1029.shinryo.index;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+import saka1029.shinryo.common.Common;
+
+public class TestRegex {
+    Logger logger = Common.logger(TestRegex.class);
+
+    static final Pattern KANJI = Pattern.compile("\\p{IsHan}+");
+    static final Pattern ZENKAKU_ALPHABET = Pattern.compile("[Ａ-Ｚａ-ｚ]+");
+    static final Pattern ZENKAKU_HIRAGANA = Pattern.compile("\\p{IsHiragana}+");
+    static final Pattern ZENKAKU_KATAKANA = Pattern.compile("\\p{IsKatakana}+");
+
+    List<String> findAll(Pattern pattern, String text) {
+        return pattern.matcher(text).results().map(m -> m.group()).toList();
+    }
+
+    @Test
+    public void testRegex() {
+        String text = "abc漢字あいうカキクＤＦＧ123ｘｙｚ４５６";
+        assertEquals(List.of("漢字"), findAll(KANJI, text));
+        assertEquals(List.of("ＤＦＧ", "ｘｙｚ"), findAll(ZENKAKU_ALPHABET, text));
+        assertEquals(List.of("あいう"), findAll(ZENKAKU_HIRAGANA, text));
+        assertEquals(List.of("カキク"), findAll(ZENKAKU_KATAKANA, text));
+    }   
+
+}
--- src/test/java/saka1029/shinryo/kuromoji/TestInvertedIndex.java
+++ src/test/java/saka1029/shinryo/kuromoji/TestInvertedIndex.java
@@ -3,12 +3,16 @@ package saka1029.shinryo.kuromoji;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.function.Consumer;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import org.junit.Test;
+
+import com.atilika.kuromoji.ipadic.Tokenizer;
 
 import saka1029.shinryo.common.Common;
 import saka1029.shinryo.common.Param;
@@ -23,15 +27,38 @@ public class TestInvertedIndex {
     static final Param param = Param.of("in", "debug/out", "06");
     static final Set<String> MAIN_NODES = Set.of("章", "部", "節", "款", "通則");
 
+    static List<String> words(String text) {
+        List<String> result = new ArrayList<>();
+        Tokenizer tokenizer = new Tokenizer() ;
+        List<com.atilika.kuromoji.ipadic.Token> tokens = tokenizer.tokenize(text.replaceAll("\\s+", ""));
+        StringBuilder noun = new StringBuilder();
+        for (com.atilika.kuromoji.ipadic.Token token : tokens)
+            // if (token.getPartOfSpeechLevel1().equals("名詞") && !token.getPartOfSpeechLevel2().equals("接尾")) {
+            if (token.getPartOfSpeechLevel1().equals("名詞")) {
+                noun.append(token.getSurface());
+            } else {
+                if (noun.length() > 0) {
+                    result.add(noun.toString());
+                    noun.setLength(0);
+                }
+            }
+        if (noun.length() > 0)
+            result.add(noun.toString());
+        return result;
+    }
+
     static void index(Node node, String url, Consumer<String> out) throws IOException {
         if (!node.isRoot()) {
             Token t = node.token;
-            if (t.type.name.equals("区分番号"))
+            if (t.type.name.equals("区分番号")) {
                 url = node.id + ".html";
-            else if (MAIN_NODES.contains(t.type.name))
+                logger.info(url);
+            } else if (MAIN_NODES.contains(t.type.name))
                 url = node.path + ".html";
             // else 親のURLを継承
-            out.accept("%s:%s %s".formatted(url, t.number, t.header + t.body.stream().collect(Collectors.joining())));
+            // out.accept("%s:%s %s".formatted(url, t.number, t.header + t.body.stream().collect(Collectors.joining())));
+            List<String> words = words(t.header + t.body.stream().collect(Collectors.joining()));
+            out.accept("%s:%s".formatted(url, words.stream().collect(Collectors.joining(" "))));
         }
         for (Node child : node.children)
             index(child, url, out);
