b6d180b25904ab80cfd80de82593714cb54ffaaf
saka1029 <saka1029@gmail.com>
2026-05-17 07:34:27 +0900

    施設基準告示の別表参照誤り訂正

--- src/main/java/saka1029/shinryo/renderer/施設基準告示本文.java
+++ src/main/java/saka1029/shinryo/renderer/施設基準告示本文.java
@@ -1,7 +1,10 @@
 package saka1029.shinryo.renderer;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import saka1029.shinryo.common.TextWriter;
@@ -14,14 +17,35 @@ public class 施設基準告示本文 extends 施設基準本文 {
         super(outDir, "k");
     }
     
+    final Set<String> 別表PATH = new HashSet<>();
     static final Pattern 別表参照 = Pattern.compile("別\\s*表\\s*第\\s*(" + Pat.漢数字の + ")");
+    static final Pattern ひとつ上の参照 = Pattern.compile("(.*)(の\\s*" + Pat.漢数字 + ")$");
     
     @Override
     public String anchor(String s, Node n) {
-        String k = n.path.replaceFirst("_.*", "");
+        // String k = n.path.replaceFirst("_.*", "");
+        String kihonTokkei = n.path.substring(0, 1);    // 1:基本, 2=特掲
         return 別表参照.matcher(s).replaceAll(m -> {
-            String b = m.group(), a = Pat.漢数字正規化(m.group(1));
-            return "<a href='k%s_b_%s.html'>%s</a>".formatted(k, a, b);
+            String all = m.group();
+            String norm = Pat.漢数字正規化(m.group(1));
+            String bpath = kihonTokkei + "_b_" + norm;
+            boolean found = 別表PATH.contains(bpath);   // 辞書にあるかどうかを調べます。
+            // System.out.printf("path=%s all=%s norm=%s bpath=%s found=%s%n", n.path, all, norm, bpath, found);
+            if (found)
+                return "<a href='k%s.html'>%s</a>".formatted(bpath, all);
+            // マッチしなかった場合、末尾の「の漢数字」を除外してみます。
+            Matcher m2 = ひとつ上の参照.matcher(all);
+            if (m2.find()) {
+                String match = m2.group(1);
+                norm = Pat.漢数字正規化(match);
+                String rest = m2.group(2);
+                bpath = kihonTokkei + "_b_" + norm;
+                found = 別表PATH.contains(bpath);
+                // System.out.printf("  path=%s all=%s+%s norm=%s bpath=%s found=%s%n", n.path, match, rest, norm, bpath, found);
+                if (found)
+                    return "<a href='k%s.html'>%s</a>%s".formatted(bpath, match, rest);
+            }
+            return all;
         });
     }
 
@@ -32,5 +56,17 @@ public class 施設基準告示本文 extends 施設基準本文 {
             link(node, level, writer, false);
         else
             text(node, level, writer);
+    }
+
+    @Override
+    public void render(Node root, String title, String outHtmlFile) throws IOException {
+        // 別表のパスを辞書「別表PATH」に追加します。
+        root.visit(n -> {
+            if (n.parent != null && n.parent.token != null && n.parent.token.type.name.equals("別表")) {
+                // System.out.printf("type=%s id=%s path=%s%n", n.token.type.name, n.id, n.path);
+                別表PATH.add(n.path);
+            }
+        });
+        super.render(root, title, outHtmlFile);
     }
 }
